d5662fc9661778c98c81da9118d10019f9b03ef8
CAMIRO <juancamilosc@hotmail.com>
2021-01-29 20:40:18 +0800

    constructor objects with JS

---
+++ JS/index.js
@@ -0,0 +1,3 @@
+var car = new Car("AWS233", new Account('Andres J', "ABC1234"))
+car.passenger = 4;
+car.printDataCar();
--- Java/Main.java
+++ Java/Main.java
@@ -2,9 +2,7 @@
 class Main {
     public static void main(String[] args) {
         System.out.println('Hola mundo');
-        Car car = new Car();
-        car.license = "AMQ123";
-        car.driver = "Andres J.";
+        Car car = new Car("AMQ123", new Account("Andres J", "AMQ123"));
         car.passenger = 4;
         car.printDataCar();
     }
