2355c98b52c476f09e9c0d53419a5de64d26564a
CAMIRO <juancamilosc@hotmail.com>
2021-01-30 10:04:51 +0800

    adding Python classes and adding gitiginre file

--- Python/main.py
+++ Python/main.py
@@ -1,15 +1,10 @@
 from car import Car
-
+from account import Account
 
 
 if __name__ == "__main__":
     print("hola mundo")
-    car = Car()
-    car.license = "AMS234"
-    car.driver = "Andres Herrera"
-    print(vars(car))
 
-    car2 = Car()
-    car2.license = "AMS766"
-    car2.driver = "Andrea Herrera"
-    print(vars(car2))
+    car = Car("AMS234", Account("Andres Herrera", "121323"))
+    print(vars(car))
+    print(vars(car.driver))
