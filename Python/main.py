baeda9b5bdf5323fe4cd54527280eee2dc8053ba
CAMIRO <juancamilosc@hotmail.com>
2021-02-01 11:25:45 +0800

    adding super and sub class python

--- Python/main.py
+++ Python/main.py
@@ -1,10 +1,25 @@
 from car import Car
+from UberX import UberX
+from UberBlack import UberBlack
 from account import Account
 
 
 if __name__ == "__main__":
-    print("hola mundo")
 
+    print("super class ")
     car = Car("AMS234", Account("Andres Herrera", "121323"))
     print(vars(car))
     print(vars(car.driver))
+    print(" ")
+
+
+    print("Sub class UberX ")
+    uberX = UberX("x",Account("Angie",12345),"chebrolet","MD")
+    print(vars(uberX))
+    print(vars(uberX.driver))
+    print(" ")
+
+    print("Sub class UberBlack ")
+    uberBlack = UberBlack("black",Account("Bob",54321),["chebrolet","mazta"],["MD","KS"])
+    print(vars(uberBlack))
+    print(vars(uberBlack.driver))
